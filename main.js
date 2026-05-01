059f0fdb693a4da8922b32f6ca2b9cf8c0a9ab68
senhorK <erickuchirradh@gmail.com>
2026-05-01 07:25:13 -0300

    premeiro

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+class Super {
+  constructor(){
+    
+    
+    this.init();
+  }
+  
+  newGame(){}
+  update(){}
+  draw(){}
+  loop = ()=>{}
+  init(){}
+}
+
+
+const main = new Super();
---
+++ projeto/pro.js
@@ -0,0 +1,9 @@
+
+
+const boxProjeto = document.querySelector(".boxProjeto");
+const boxProBtn = document.querySelector(".boxProjeto button");
+      boxProBtn.addEventListener("click", ()=>{
+        boxProjeto.classList.remove("ativo")
+      })
+
+
